2552ba9e373f45b042a3c3895bf586e4db917de5
Florian <florian@technotro.com>
2023-11-19 16:18:05 +0300

    feat(finite-field.ts): implement finite field arithmetic operations and field class
    This commit adds the implementation of finite field arithmetic operations in the `finite-field.ts` file. The following operations are implemented:
    - `mod(a, p)`: calculates the modulo of `a` with respect to `p`
    - `div(a, b, p)`: calculates the division of `a` by `b` modulo `p`
    - `add(a, b, p)`: calculates the addition of `a` and `b` modulo `p`
    - `sub(a, b, p)`: calculates the subtraction of `b` from `a` modulo `p`
    - `mul(a, b, p)`: calculates the multiplication of `a` and `b` modulo `p`
    - `pow(a, n, p)`: calculates `a` raised to the power of `n` modulo `p`
    - `inverse(a, p)`: calculates the multiplicative inverse of `a` modulo `p`
    - `random(p)`: generates a random number in the range [0, p)
    - `isSquare(x, p)`: checks if `x` is a square modulo `p`
    - `sqrt(n, p)`: implementation of the Tonelli-Shanks algorithm to calculate the square root of `n` modulo `p`
    
    Additionally, a `Field` class is defined using the `createField` function. The `Field` class represents an element in the finite field with modulus `p`. It provides methods for performing arithmetic operations on field elements, such as addition, subtraction, multiplication, exponentiation, and division. The class also includes utility methods for creating field elements from values, checking if a value is a field element, and comparing field elements.
    
    The `Field` class is exported along with the `FieldClass` type, which represents the constructor of the `Field` class.

---
+++ src/finite-field.ts
@@ -0,0 +1,194 @@
+import { randomBytes } from "crypto";
+
+export { Field, FieldClass, createField };
+
+function mod(a: bigint, p: bigint) {
+  let x = a % p;
+  if (x < 0) return a + p;
+  return x;
+}
+
+function div(a: bigint, b: bigint, p: bigint) {
+  let ainv = inverse(a, p);
+  if (ainv === undefined) return; // we assume p is prime, therefor every element should be invertible gcd(a, p) = 1
+  return mod(b * ainv, p);
+}
+
+function add(a: bigint, b: bigint, p: bigint) {
+  return mod(a + b, p);
+}
+
+function sub(a: bigint, b: bigint, p: bigint) {
+  return mod(a - b, p);
+}
+
+function mul(a: bigint, b: bigint, p: bigint) {
+  return mod(a * b, p);
+}
+
+// a^n mod p
+function pow(a: bigint, n: bigint, p: bigint) {
+  a = mod(a, p);
+  let x = 1n;
+  for (; n > 0n; n >>= 1n) {
+    if (n & 1n) x = mod(x * a, p);
+    a = mod(a * a, p);
+  }
+  return x;
+}
+
+// inv(a) in Fp with EGCD
+function inverse(a: bigint, p: bigint) {
+  a = mod(a, p);
+  if (a === 0n) return undefined;
+  let b = p;
+  let x = 0n;
+  let y = 1n;
+  let u = 1n;
+  let v = 0n;
+  while (a !== 0n) {
+    let q = b / a;
+    let r = mod(b, a);
+    let m = x - u * q;
+    let n = y - v * q;
+    b = a;
+    a = r;
+    x = u;
+    y = v;
+    u = m;
+    v = n;
+  }
+  if (b !== 1n) return undefined;
+  return mod(x, p);
+}
+
+function random(p: bigint) {
+  let size = Math.floor(p.toString(2).length / 8);
+  while (true) {
+    let x = BigInt("0x" + randomBytes(size).toString("hex"));
+    if (x < p) return x;
+  }
+}
+
+function isSquare(x: bigint, p: bigint) {
+  if (x === 0n) return true;
+  return pow(x, (p - 1n) / 2n, p) === 1n;
+}
+
+/*
+Tonelli-Shanks algorithm.
+Input: 
+  p, a prime 
+  n, the n in r^2 = n
+Output:
+  r, the r in r^2 = n
+*/
+function sqrt(n: bigint, p: bigint) {}
+
+const createField = (p: bigint) =>
+  class Field {
+    value: bigint;
+
+    static p: bigint = p;
+
+    constructor(x: bigint | Field) {
+      this.value = mod(Field.isField(x) ? x.value : x, Field.p);
+    }
+
+    static get modulus() {
+      return this.p;
+    }
+
+    toBigint() {
+      return this.value;
+    }
+
+    toString() {
+      return this.value.toString();
+    }
+
+    static zero() {
+      return new Field(0n);
+    }
+
+    static one() {
+      return new Field(1n);
+    }
+
+    static random() {
+      return new Field(random(Field.p));
+    }
+
+    add(x: Field | bigint): Field {
+      return new Field(
+        add(this.value, Field.isField(x) ? x.value : x, Field.p)
+      );
+    }
+
+    sub(x: Field | bigint): Field {
+      return new Field(
+        sub(this.value, Field.isField(x) ? x.value : x, Field.p)
+      );
+    }
+
+    mul(x: Field | bigint): Field {
+      return new Field(
+        mul(this.value, Field.isField(x) ? x.value : x, Field.p)
+      );
+    }
+
+    square(): Field {
+      return new Field(pow(this.value, 2n, Field.p));
+    }
+
+    pow(x: Field | bigint): Field {
+      return new Field(
+        pow(this.value, Field.isField(x) ? x.value : x, Field.p)
+      );
+    }
+
+    div(x: Field | bigint) {
+      return this.mul(Field.from(x).inverse());
+    }
+
+    inverse() {
+      let x = inverse(this.value, Field.p)!;
+      if (x === undefined) throw Error("Inverse does not exist");
+      return new Field(x);
+    }
+
+    equals(a: Field | bigint) {
+      return Field.isField(a) ? a.value === this.value : a === this.value;
+    }
+
+    lessThan(a: Field | bigint) {
+      return Field.isField(a) ? a.value > this.value : a > this.value;
+    }
+
+    lessThanOrEqual(a: Field | bigint) {
+      return Field.isField(a) ? a.value >= this.value : a >= this.value;
+    }
+
+    greaterThan(a: Field | bigint) {
+      return !this.lessThan(a);
+    }
+
+    greaterThanOrEqual(a: Field | bigint) {
+      return !this.lessThanOrEqual(a);
+    }
+
+    inRange() {
+      return this.value <= Field.p;
+    }
+
+    static from(x: bigint | Field) {
+      return new Field(x);
+    }
+
+    static isField(x: Field | bigint): x is Field {
+      return typeof x !== "bigint";
+    }
+  };
+
+type Field = InstanceType<ReturnType<typeof createField>>;
+type FieldClass = ReturnType<typeof createField>;
